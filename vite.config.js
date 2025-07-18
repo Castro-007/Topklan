4e8d97f12773d9baf4a38b012223a4d097e29a6d
Castro-007 <sammythompson202@gmail.com>
2025-07-18 15:00:29 +0100

    added allowedHost

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // 
+    host: '0.0.0.0', 
+    allowedHosts: ['topklan.onrender.com'],
     port: process.env.PORT || 5173,
   },
   assetsInclude: ['**/*.PNG'],
